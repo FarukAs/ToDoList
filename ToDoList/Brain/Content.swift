b4a1059831fce8c341da9eb0fc9322bc13d77585
FarukAs <omeraso1998@gmail.com>
2022-12-09 11:58:13 +0300

    codes were made concise and table view recell problem was solved

---
+++ ToDoList/Brain/Content.swift
@@ -0,0 +1,12 @@
+//
+//  Content.swift
+//  ToDoList
+//
+//  Created by Şeyda Soylu on 8.12.2022.
+//
+
+import Foundation
+class item {
+    var title: String = ""
+    var done: Bool = false
+}
--- ToDoList/TodoListViewController.swift
+++ ToDoList/TodoListViewController.swift
@@ -9,42 +9,47 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     let userdefault = UserDefaults.standard
-    var array = ["Alınacaklar", "Arabalar", "Alışveriş listesi"]
+    var array = [item] ()
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let item = userdefault.array(forKey: "array") as? [String] {
-            array = item
-        }
+        let newitem = item()
+        newitem.title = "Alınacaklar"
+        newitem.done = true
+        array.append(newitem)
+    
+        
+        let newitem1 = item()
+        newitem1.title = "Arabalar"
+        array.append(newitem1)
+        
+        let newitem2 = item()
+        newitem2.title = "Alışveriş listesi"
+        array.append(newitem2)
+
+ //       if let item = userdefault.array(forKey: "array") as? [String] {
+ //           array = [item]
+ //       }
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(indexPath.row)
+        
+        array[indexPath.row].done = !array[indexPath.row].done
+        
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
-            if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-                tableView.cellForRow(at: indexPath)?.accessoryType = .none
-            } else {
-                tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-            }
-        
-        
     }
-    override func tableView(_ tableView: UITableView, didHighlightRowAt indexPath: IndexPath) {
-        UIView.animate(withDuration: 0.4) {
-            if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-                tableView.cellForRow(at: indexPath)?.backgroundColor = .white
-            } else {
-                tableView.cellForRow(at: indexPath)?.backgroundColor = UIColor(red: 34/255, green: 139/255, blue: 34/255, alpha: 0.8)
-            }
-        }
-    }
-
-    
+   
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return array.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
-        cell.textLabel?.text = array[indexPath.row]
+        let item = array[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done  ? .checkmark : .none
+        
         return cell
     }
 
@@ -53,7 +58,10 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Todo item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { [self] (action) in
             if textField.text != nil {
-                array.append(textField.text!)
+                
+                let newitem = item()
+                newitem.title = (textField.text)!
+                array.append(newitem)
                 tableView.reloadData()
                 self.userdefault.set(self.array, forKey: "array")
             }
